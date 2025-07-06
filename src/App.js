023924ea7f46001d2836b1f9c3dd6fdc1eab957c
Aman Raj <amanraj5687@gmail.com>
2025-07-07 01:54:44 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null); // useRef to store interval ID
+
+  const stop = () => {
+    if (isRunning) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+      setIsRunning(false);
+    }
+  };
+
+  const start = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+  };
+
+  const reset = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setIsRunning(false);
+    setTime(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stop Watch</h1>
+      <p>time: {new Date(time * 1000).toISOString().substr(11, 8)}</p>
+      <div>
+        <button onClick={isRunning ? stop : start}>{isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
